61108e22518e7d9c1146e893bac92f75a3542894
MR <759823845@qq.com>
2023-07-31 01:04:06 +0800

    update

--- maneu/urls.py
+++ maneu/urls.py
@@ -20,4 +20,5 @@ from maneu import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('getPhoneCall/', views.getPhoneCall, name='getPhoneCall')
 ]
--- maneu/views.py
+++ maneu/views.py
@@ -15,3 +15,19 @@ def login(request):
             }
     reecho = requests.get(url, data)
     return HttpResponse(reecho, reecho.json())
+
+
+def getPhoneCall(request):
+    getAccessTokenUrl = 'https://api.weixin.qq.com/cgi-bin/token'
+    data = {"appid": "wxf48b774de9be5613",
+            "secret": "9a7ac5730b249c8ccc8a2b410631935b",
+            "grant_type": "client_credential"
+            }
+    access_token = requests.get(getAccessTokenUrl, data).json()
+    print(access_token)
+    url = 'https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token='+access_token['access_token']
+    # data2 = {"code": request.GET.get('code')}
+    data2 = {"code": '5a8c07a4d0e4fe4c10d8c55301f5f262db226322b33d3d5003b5345a7b2832f6'}
+
+    phoneCall = requests.post(url, data2).json()
+    print(phoneCall)
