6315d10bb6545e2385012e893bd77b7b9db45747
wumq <759823845@qq.com>
2024-07-23 02:02:39 +0800

    update

--- maneu/views.py
+++ maneu/views.py
@@ -48,9 +48,10 @@ def getOrderDetail(request):
     if(request.GET.get('code') == ''):
         content = {'status': False, 'message': 'code is none', 'data': {}}
     else:
-        data = ManeuOrder.objects.filter(id=request.GET.get('code')).first()
-        store = ManeuStore.objects.filter(id=data.store_id).first()
-        content = {'status': True, 'message': 'success', 'content': {'time': data.time, 'data': json.loads(store.content)}}
+        order = ManeuOrderV2.objects.filter(id=request.GET.get('code')).first()
+        store = ManeuStore.objects.filter(id=order.store_id).first()
+        vision = ManeuVision.objects.filter(id=order.vision_id).first()
+        content = {'status': True, 'message': 'success', 'content': {'time': order.time, vision: json.loads(vision.content), 'store': json.loads(store.content)}}
     return JsonResponse(content)
 
 
