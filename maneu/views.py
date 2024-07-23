9083efa16129889f2ef0f09d388fe03301f193d5
wumq <759823845@qq.com>
2024-07-24 03:22:45 +0800

    update

--- maneu/views.py
+++ maneu/views.py
@@ -41,35 +41,32 @@ def get_list(request):
 
 
 def get_detail(request):
-    if request.GET.get('code') == '':
-        content = {'status': False, 'message': 'code is none'}
+    if request.GET.get('text') == "Order":
+        try:
+            order = ManeuOrderV2.objects.filter(id=request.GET.get('code')).first()
+            store = ManeuStore.objects.filter(id=order.store_id).first()
+            vision = ManeuVision.objects.filter(id=order.vision_id).first()
+            content = {'status': True,
+                       'message': 'success',
+                        'time': order.time,
+                       'store': json.loads(store.content),
+                       'vision': json.loads(vision.content)}
+        except Exception as e:
+                content = {'status': False, 'message': e}
+    elif request.GET.get('text') == "Service":
+        try:
+            data = ManeuService.objects.filter(guess_id=request.GET.get('code')).order_by('-time').first()
+            content = {'status': True, 'message': 'success', 'content': list(data)}
+        except Exception as e:
+            content = {'status': False, 'message': e}
+    elif request.GET.get('text') == "Refraction":
+        try:
+            data = json.loads(ManeuRefraction.objects.filter(id=request.GET.get('code')).first().content)
+            content = {'status': True, 'message': 'success', 'content': data}
+        except Exception as e:
+            content = {'status': False, 'message': e}
     else:
-        if request.GET.get('text') == "Order":
-            try:
-                order = ManeuOrderV2.objects.filter(id=request.GET.get('code')).first()
-                store = ManeuStore.objects.filter(id=order.store_id).first()
-                vision = ManeuVision.objects.filter(id=order.vision_id).first()
-                content = {'status': True,
-                           'message': 'success',
-                           'time': order.time,
-                           'store': json.loads(store.content),
-                           'vision': json.loads(vision.content)}
-            except Exception as e:
-                content = {'status': False, 'message': e}
-        elif request.GET.get('text') == "Service":
-            try:
-                data = ManeuService.objects.filter(guess_id=request.GET.get('code')).order_by('-time').first()
-                content = {'status': True, 'message': 'success', 'content': list(data)}
-            except Exception as e:
-                content = {'status': False, 'message': e}
-        elif request.GET.get('text') == "Refraction":
-            try:
-                data = json.loads(ManeuRefraction.objects.filter(id=request.GET.get('code')).first().content)
-                content = {'status': True, 'message': 'success', 'content': data}
-            except Exception as e:
-                content = {'status': False, 'message': e}
-        else:
-            content = {'status': False, 'message': 'code is none'}
+        content = {'status': False, 'message': 'code is none'}
     return JsonResponse(content)
 
 
