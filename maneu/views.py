43364ed44c6b43326231010d1fcc27e068ab5f06
wumq <759823845@qq.com>
2024-09-05 18:15:20 +0800

    1

--- maneu/views.py
+++ maneu/views.py
@@ -74,8 +74,7 @@ def get_detail(request):
 
 def sendSMS(request):
     # Please ensure that the environment variables ALIBABA_CLOUD_ACCESS_KEY_ID and ALIBABA_CLOUD_ACCESS_KEY_SECRET are set.
-    credentials = AccessKeyCredential(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'],
-                                      os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'])
+    credentials = AccessKeyCredential()
     # use STS Token
     # credentials = StsTokenCredential(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'], os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'], os.environ['ALIBABA_CLOUD_SECURITY_TOKEN'])
     client = AcsClient(region_id='cn-shenzhen', credential=credentials)
