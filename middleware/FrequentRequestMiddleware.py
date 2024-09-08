c97ea2821e0e536f452175681318d8697df5f96e
wumq <759823845@qq.com>
2024-09-09 03:49:08 +0800

    delete

---
+++ middleware/FrequentRequestMiddleware.py
@@ -0,0 +1,29 @@
+from django.utils.deprecation import MiddlewareMixin
+from common import common
+import time
+
+
+class FrequentRequestMiddleware(MiddlewareMixin):
+    """
+    频繁请求中间件类
+    每秒只能请求1次
+    如果同一个session 每秒请求超过一次就会延迟10秒
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        print("--频繁请求中间件类启动--")
+
+    def process_request(self, request):
+        current_time = common.current_time()
+        try:
+            request_time = request.session['request_time']
+        except:
+            request.session['request_time'] = current_time
+            request_time = current_time
+        if current_time == request_time:
+            time.sleep(10)
+            print('频繁请求')
+        else:
+            request.session['request_time'] = current_time
+            print('正常请求')
