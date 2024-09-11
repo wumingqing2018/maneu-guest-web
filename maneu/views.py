1c2497afb87da67747b0346fb1c711aa210bf0d1
wumq <759823845@qq.com>
2024-09-12 04:17:29 +0800

    delete

--- maneu/views.py
+++ maneu/views.py
@@ -1,17 +1,14 @@
 import json
+import os
+import re
 
+from aliyunsdkcore.auth.credentials import AccessKeyCredential
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkdysmsapi.request.v20170525.SendSmsRequest import SendSmsRequest
 from django.http import JsonResponse
 from django.shortcuts import render
 
 from maneu.models import *
-import re
-import os
-from aliyunsdkcore.client import AcsClient
-from aliyunsdkcore.acs_exception.exceptions import ClientException
-from aliyunsdkcore.acs_exception.exceptions import ServerException
-from aliyunsdkcore.auth.credentials import AccessKeyCredential
-from aliyunsdkcore.auth.credentials import StsTokenCredential
-from aliyunsdkdysmsapi.request.v20170525.SendSmsRequest import SendSmsRequest
 
 
 def index(request):
@@ -84,23 +81,25 @@ def sendsms(request):
     pattern = re.compile(r'^1[3-9]\d{9}$')
     code = str(request.GET.get('code'))
     if pattern.match(code) is None:
-            # Please ensure that the environment variables ALIBABA_CLOUD_ACCESS_KEY_ID and ALIBABA_CLOUD_ACCESS_KEY_SECRET are set.
-            credentials = AccessKeyCredential(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'], os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'])
-            # use STS Token
-            # credentials = StsTokenCredential(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'], os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'], os.environ['ALIBABA_CLOUD_SECURITY_TOKEN'])
-            client = AcsClient(region_id='cn-shenzhen', credential=credentials)
+        # Please ensure that the environment variables ALIBABA_CLOUD_ACCESS_KEY_ID and ALIBABA_CLOUD_ACCESS_KEY_SECRET are set.
+        credentials = AccessKeyCredential(os.environ['ALIBABA_CLOUD_ACCESS_KEY_IDv'],
+                                          os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'])
+        # use STS Token
+        # credentials = StsTokenCredential(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'], os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'], os.environ['ALIBABA_CLOUD_SECURITY_TOKEN'])
+        client = AcsClient(region_id='cn-shenzhen', credential=credentials)
 
-            request = SendSmsRequest()
-            request.set_accept_format('json')
+        request = SendSmsRequest()
+        request.set_accept_format('json')
 
-            request.set_SignName("徕可")
-            request.set_TemplateCode("SMS_471990239")
-            request.set_PhoneNumbers("13640651582")
-            request.set_TemplateParam("{\"code\":\"1234\"}")
+        request.set_SignName("徕可")
+        request.set_TemplateCode("SMS_471990239")
+        request.set_PhoneNumbers("13640651582")
+        request.set_TemplateParam("{\"code\":\"1234\"}")
 
-            response = client.do_action_with_exception(request)
-            # python2:  print(response)
-            print(str(response, encoding='utf-8'))
+        response = client.do_action_with_exception(request)
+        # python2:  print(response)
+        # print(str(response, encoding='utf-8'))
+        content = response
     else:
         content = {'status': False, 'message': 'code is none', 'data': {}}
-    return JsonResponse('1')
+    return JsonResponse(content)
