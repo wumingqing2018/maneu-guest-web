04bf9cc8ae0b8512f0749d5ec6f376b87955e2e2
wumq <759823845@qq.com>
2024-10-03 00:58:10 +0800

    update

--- maneu/views.py
+++ maneu/views.py
@@ -51,8 +51,8 @@ def get_list(request):
         else:
             content = {'status': False, 'message': '100002', 'content': {}}
     else:
-        content = ['3.gif', '1mcjs.jpg', '1xzy.jpg', '1njj.jpg']
-        content = {'status': True, 'message': '100000', 'content': content}
+        data = ['3.gif', '1mcjs.jpg', '1xzy.jpg', '1njj.jpg']
+        content = {'status': True, 'message': '100000', 'content': data}
 
     return JsonResponse(content)
 
