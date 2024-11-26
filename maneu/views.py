7e2075e53b7d47b788f846b65dfb692a8a86bc36
wumq <759823845@qq.com>
2024-11-27 03:06:48 +0800

    添加可视化功能

--- maneu/views.py
+++ maneu/views.py
@@ -147,7 +147,7 @@ def get_visual(request):
         for guest in guest_list:
             report_list = ManeuReport.objects.filter(guest_id=guest.id).order_by('-time').all()
             for report in report_list:
-                data.extend(report.time)
+                data.extend(str(report.time))
         return JsonResponse({'status': True, 'message': '', 'content': data})
 
     return JsonResponse({'status': False, 'message': '', 'content': {}})
