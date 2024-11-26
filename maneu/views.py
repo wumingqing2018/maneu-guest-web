1b6e0177747cd95080a46f98c66872e65a92f4ec
wumq <759823845@qq.com>
2024-11-27 03:27:05 +0800

    添加可视化功能

--- maneu/views.py
+++ maneu/views.py
@@ -150,6 +150,9 @@ def get_visual(request):
             for report in report_list:
                 content = json.loads(report.content)
                 print(content)
+                print(content.OD.SPH)
+                print(content.OS.SPH)
+
                 OD.extend(content.OD.SPH)
                 OS.extend(content.OS.SPH)
 
