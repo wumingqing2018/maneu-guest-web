03662b5297829afc957891661b051df33efe673b
wumq <759823845@qq.com>
2024-11-28 16:16:43 +0800

    添加可视化功能

--- maneu/views.py
+++ maneu/views.py
@@ -97,7 +97,7 @@ def get_list(request):
 
     if code:
         guest = ManeuGuest.objects.filter(phone=code).all().values('id')
-
+        print(guest)
         if request.GET.get('text') == "Order":
             for i in guest:
                 data.extend(ManeuOrder.objects.filter(guest_id=i).order_by('-time').all().values('id', 'time'))
