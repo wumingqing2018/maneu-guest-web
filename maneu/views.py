47adc16393f64859972be8876bbbdb40fd5be889
wumq <759823845@qq.com>
2024-12-02 16:11:39 +0800

    添加可视化功能

--- maneu/views.py
+++ maneu/views.py
@@ -118,7 +118,7 @@ def get_list(request):
             for i in report:
                 content = json.loads(i.content)
                 id.append(i.id)
-                time.append(i.time.strftime("%Y-%M-%D"))
+                time.append(i.time.strftime("%Y-%m-%d"))
                 if content['OD']['AL']:
                     OD_AL.append(float(content['OD']['AL']))
                 else:
