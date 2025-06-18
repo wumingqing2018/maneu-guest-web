e87c15319496f45a7181ddfda52d9323d7499324
wumq <759823845@qq.com>
2025-06-19 06:04:24 +0800

    添加可视化功能

--- maneu/views.py
+++ maneu/views.py
@@ -77,7 +77,7 @@ def get_index(request):
 def get_list(request):
     token = verify.is_token(request.GET.get('token'))
     text = verify.is_code(request.GET.get('text'))
-    print(type(request.session.get('token')), request.session.get('token'), request.session)
+    print(type(request.session.get('token')), request.session.get('token'), dict(request.session))
 
     if token == request.session.get('token'):
         guest = list(ManeuGuest.objects.filter(phone=request.session['guest_id']).all().values_list('id', flat=True))
