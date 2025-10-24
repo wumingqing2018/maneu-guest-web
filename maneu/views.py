3e75e339fee5e703767f8b66a0c59f75463057c0
wumq <759823845@qq.com>
2025-10-25 05:49:46 +0800

    添加可视化功能

--- maneu/views.py
+++ maneu/views.py
@@ -10,6 +10,10 @@ from django.forms.models import model_to_dict
 
 
 def index(request):
+    ManeuGuest.objects.filter(admin_id="36483774080401481140071775853431").update(admin_id='60fdfea6-2d3f-11ed-b7f2-00163e02ac92')
+    ManeuOrder.objects.filter(admin_id="36483774080401481140071775853431").update(admin_id='60fdfea6-2d3f-11ed-b7f2-00163e02ac92')
+    ManeuBuffer.objects.filter(admin_id="36483774080401481140071775853431").update(admin_id='60fdfea6-2d3f-11ed-b7f2-00163e02ac92')
+
     return render(request, 'index.html')
 
 
